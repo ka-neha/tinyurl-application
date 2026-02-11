4cd51783f530b6ff0c7fc085bdccd77227545390
Nehadivami <nehak.a7777@gmail.com>
2026-02-11 14:35:05 +0530

    environment setting for rendering ui

--- UrlShortener.Api/Program.cs
+++ UrlShortener.Api/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular", policy =>
     {
         policy
-            .AllowAnyOrigin()
+            .withOrigins("http://localhost:4200", "https://tinyurl-ui-rldy.onrender.com")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
--- src/app/core/services/url.service.ts
+++ src/app/core/services/url.service.ts
@@ -2,34 +2,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ShortUrl } from '../models/short-url.model';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UrlService {
 
-  private api = 'https://localhost:5001/api/shorturls';
-
+ private apiBase = environment.apiUrl;
   constructor(private http: HttpClient) {}
 
   create(data: Partial<ShortUrl>): Observable<ShortUrl> {
-    return this.http.post<ShortUrl>(this.api, data);
+    return this.http.post<ShortUrl>(this.apiBase, data);
   }
 
   getAll(): Observable<ShortUrl[]> {
-    return this.http.get<ShortUrl[]>(this.api);
+    return this.http.get<ShortUrl[]>(this.apiBase);
   }
 
   search(term: string): Observable<ShortUrl[]> {
-    return this.http.get<ShortUrl[]>(`${this.api}/search/${term}`);
+    return this.http.get<ShortUrl[]>(`${this.apiBase}/search/${term}`);
   }
 
   delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.api}/${id}`);
+    return this.http.delete<void>(`${this.apiBase}/${id}`);
   }
 
   incrementClicks(id: number): Observable<void> {
-    return this.http.post<void>(`${this.api}/${id}/click`, {});
+    return this.http.post<void>(`${this.apiBase}/${id}/click`, {});
   }
 
 }
