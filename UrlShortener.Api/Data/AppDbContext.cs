5743c7fbf38de7dcf13242f92014ac68b78e12a2
Nehadivami <nehak.a7777@gmail.com>
2026-02-11 12:04:32 +0530

    backend implementation and updated readme and gitignore file

---
+++ UrlShortener.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Api.Models;
+
+namespace UrlShortener.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+    }
+}
